519ebaea52f9f818fd240cb8cb740989edc0d62b
FTVIU0 <HGQ577068802@163.com>
2015-12-13 00:32:16 +0800

    调用AcceptTcpClient()来获取与一个客户端连接

--- ServerConsole/Program.cs
+++ ServerConsole/Program.cs
@@ -13,11 +13,21 @@ namespace ServerConsole
         static void Main(string[] args)
         {
             Console.WriteLine("Server is running ... ");
+            // 获得IPAddress对象的另外几种常用方法：
+            //IPAddress ip = IPAddress.Parse("127.0.0.1");
+            //IPAddress ip = Dns.GetHostEntry("localhost").AddressList[0];
             IPAddress ip = new IPAddress(new byte[] { 192, 168, 253, 1 });
             TcpListener listener = new TcpListener(ip, 8500);
 
             listener.Start();           // 开始侦听
             Console.WriteLine("Start Listening ...");
+
+            // 获取一个连接，中断方法
+            TcpClient remoteClient = listener.AcceptTcpClient();
+
+            // 打印连接到的客户端信息
+            Console.WriteLine("Client Connected！{0} <-- {1}",
+               remoteClient.Client.LocalEndPoint, remoteClient.Client.RemoteEndPoint);
 
             Console.WriteLine("\n\n输入\"Q\"键退出。");
             ConsoleKey key;
