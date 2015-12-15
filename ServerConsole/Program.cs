da1f638aff30d803977fe2ddb99e9d726832315c
NLTE <HGQ577068802@163.com>
2015-12-15 09:58:34 +0800

    服务端将处理多个客户端的一条请求

--- ServerConsole/Program.cs
+++ ServerConsole/Program.cs
@@ -23,40 +23,26 @@ namespace ServerConsole
             listener.Start();           // 开始侦听
             Console.WriteLine("Start Listening ...");
 
-            //获取一个连接，中断方法
-            TcpClient remoteClient = listener.AcceptTcpClient();
-            // 打印连接到的客户端信息
-            Console.WriteLine("Client Connected！{0} <-- {1}",
-                remoteClient.Client.LocalEndPoint, remoteClient.Client.RemoteEndPoint);
-
-            //获得流并写入buffer中
-            NetworkStream streamToClient = remoteClient.GetStream();
-            byte[] buffer = new byte[BufferSize];
-            int bytesRead = streamToClient.Read(buffer, 0, BufferSize);
-            Console.WriteLine("Reading data, {0} bytes ...", bytesRead);
-
-            // 获得请求的字符串
-            String msg = Encoding.Unicode.GetString(buffer, 0, bytesRead);
-            Console.WriteLine("Received: {0}", msg);
-
-            // 按Q退出
-            Console.WriteLine("\n\n输入\"Q\"键退出。");
-            ConsoleKey key;
+            //使用一个do/while循环，并将listener.AcceptTcpClient()方法和TcpClient.GetStream().Read()方法都放在这个循环以内，
+            //那么服务端将可以处理多个客户端的一条请求
             do
             {
-                key = Console.ReadKey(true).Key;
-            } while (key != ConsoleKey.Q);
-
-            //do...while循环接受多个客户端的连接信息
-            /*while (true)//接收到多个客户端的连接
-            {
-                // 获取一个连接，同步方法
+                //获取一个连接，中断方法
                 TcpClient remoteClient = listener.AcceptTcpClient();
                 // 打印连接到的客户端信息
                 Console.WriteLine("Client Connected！{0} <-- {1}",
                     remoteClient.Client.LocalEndPoint, remoteClient.Client.RemoteEndPoint);
-            }*/
 
+                //获得流并写入buffer中
+                NetworkStream streamToClient = remoteClient.GetStream();
+                byte[] buffer = new byte[BufferSize];
+                int bytesRead = streamToClient.Read(buffer, 0, BufferSize);
+                Console.WriteLine("Reading data, {0} bytes ...", bytesRead);
+
+                // 获得请求的字符串
+                String msg = Encoding.Unicode.GetString(buffer, 0, bytesRead);
+                Console.WriteLine("Received: {0}", msg);
+            } while (true);
         }
     }
 }
